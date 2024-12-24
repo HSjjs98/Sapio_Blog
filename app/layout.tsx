ec1fc71f3cf8bf5aa17fa873d8054b6127a6cb0f
James <dawn147@snu.ac.kr>
2024-12-24 17:04:26 +0900

    settings: initial project settings

---
+++ app/layout.tsx
@@ -0,0 +1,68 @@
+import './global.css';
+
+import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
+
+import Footer from './components/footer';
+import { Navbar } from './components/nav';
+import { baseUrl } from './sitemap';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Next.js Portfolio Starter',
+    template: '%s | Next.js Portfolio Starter',
+  },
+  description: 'This is my portfolio.',
+  openGraph: {
+    title: 'My Portfolio',
+    description: 'This is my portfolio.',
+    url: baseUrl,
+    siteName: 'My Portfolio',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
+
+const cx = (...classes) => classes.filter(Boolean).join(' ');
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cx(
+        'bg-white text-black dark:bg-black dark:text-white',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
+      <body className="mx-4 mt-8 max-w-xl antialiased lg:mx-auto">
+        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
+      </body>
+    </html>
+  );
+}
